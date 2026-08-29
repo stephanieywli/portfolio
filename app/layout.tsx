825127ef0900b402175f374c850d37015bda8c74
Stephanie Li <stephanieyw.li@gmail.com>
2026-08-29 19:25:53 -0400

    strip legacy components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Garamond, Neue } from "@/utils/customFonts";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
-import SmoothScroll from "../components/layout/SmoothScroll";
-import Background from "@/components/layout/background";
-import Gradient from "@/components/layout/gradient";
-import Nav from "@/components/layout/nav";
+import { Atkinson_Hyperlegible_Mono } from "next/font/google";
 
 import "./globals.css";
 
-export const metadata: Metadata = {
+import AsciiFlower from "@/components/layout/asciiFlower";
+
+const metadata: Metadata = {
   title: "Stephanie Li",
   description:
     "Personal portfolio created by Stephanie Li. Powered by Next.js, Typescript, and Tailwind CSS.",
@@ -24,6 +20,11 @@ export const metadata: Metadata = {
   },
 };
 
+const mono = Atkinson_Hyperlegible_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -32,14 +33,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${Garamond.variable} ${Neue.variable} bg-off-white text-off-white font-neue font-normal text-[0.9rem] sm:max-4k:text-[1.1rem] 4k:text-[1.65rem] antialiased select-none overscroll-none overflow-x-hidden relative`}
+        className={`${mono.variable} bg-off-white font-mono text-black text-[0.9rem] sm:max-4k:text-[1.1rem] 4k:text-[1.65rem] antialiased select-none overscroll-none overflow-x-hidden relative`}
       >
-        <SpeedInsights />
-        <Analytics />
-        <Gradient />
-        <Background />
-        <Nav />
-        <SmoothScroll>{children}</SmoothScroll>
+        <AsciiFlower />
+        {children}
       </body>
     </html>
   );
--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,69 +1,7 @@
-"use client";
-import React from "react";
-import Link from "next/link";
-import { DateTime } from "luxon";
-import { useState, useEffect } from "react";
-import { useLenis } from "@studio-freight/react-lenis";
-import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
-
-const links = [
-  { href: "/", label: "Projects,", target: "#projects-section" },
-  { href: "/about", label: "About,", target: "top" },
-  { href: "", label: "Contact", target: "bottom" },
-];
-
-const Nav = () => {
-  const [seconds, setSeconds] = useState("0:00:00 NA");
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds(() =>
-        DateTime.now()
-          .setZone("America/New_York")
-          .setLocale("en")
-          .toLocaleString(DateTime.TIME_WITH_SECONDS)
-      );
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const lenis = useLenis(({ scroll }) => {});
-  const path = usePathname();
-
+export default function Nav() {
   return (
-    <div className="flex flex-row w-screen px-4 4k:px-10 py-3 backdrop-blur-[0.75px] justify-between bg-green bg-opacity-10 uppercase tracking-wider sm:max-4k:text-[1rem] 4k:text-[1.5rem] border-b fixed z-50">
-      <Link href="/" className="w-fit">
-        <p className="hidden md:block">PORTFOLIO 24&apos; ©</p>
-        <p className="md:hidden">SYL 24&apos; ©</p>
-      </Link>
-      <div className="hidden sm:flex w-fit">
-        Toronto, ON&nbsp;&nbsp;•&nbsp;&nbsp;{seconds}
-      </div>
-      <div className="flex flex-row w-fit gap-2">
-        {links.map((link) => (
-          <Link
-            key={link.href}
-            className="relative hover:opacity-70 hover:italic group transition"
-            href={link.href}
-            onClick={() =>
-              lenis.scrollTo(link.target, { lerp: 0.1, lock: true })
-            }
-          >
-            {link.label}
-            {link.href === path && (
-              // line change
-              <motion.span className="block w-[95%] border-b-[1px] -mt-1"></motion.span>
-            )}
-            {link.href !== path && (
-              // underline
-              <span className="block max-w-0 group-hover:max-w-[90%] transition-all duration-500 h-[1px] bg-off-white -mt-1"></span>
-            )}
-          </Link>
-        ))}
-      </div>
+    <div className="flex justify-end w-screen tracking-wider border-y bottom-0 fixed z-50">
+      <p className="text-sm py-2 px-4">syl portfolio 2026 ©</p>
     </div>
   );
-};
-
-export default Nav;
+}
