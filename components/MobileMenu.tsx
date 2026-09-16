59f06d935b47657c20a178daa6c039fbe063bdcf
Stephanie Li <stephanieyw.li@gmail.com>
2026-09-16 17:17:22 -0400

    tweak ui

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -30,7 +30,7 @@ export const MobileMenu = ({ selected, onSelect }: MobileMenuProps) => (
               onClick={() => onSelect(page)}
               className={
                 isSelected
-                  ? "font-bold text-accent"
+                  ? "font-bold text-ink"
                   : "text-ink/60 hover:text-ink transition-colors hover:cursor-pointer"
               }
             >
