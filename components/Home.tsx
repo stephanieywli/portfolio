3f310726be272c9e41c79024bdb270b1335aa74e
Stephanie Li <stephanieyw.li@gmail.com>
2026-09-15 20:10:59 -0400

    typewriter effect

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,34 +1,44 @@
-import Link from "next/link";
+"use client";
 
-export const Home = () => (
-  <>
-    {" "}
-    <p className="text-sm uppercas bg-black uppercase w-fit text-off-white px-1 font-semibold mb-2">
-      stephanie li
-    </p>
-    <p className="text-sm">
-      <b className="font-semibold">in summary:</b> a software developer & cat
-      mom
-    </p>
-    <p className="text-sm">
-      <b className="font-semibold">main proficiencies:</b> typescript, react,
-      python, sql
-    </p>{" "}
-    <p className="text-sm">
-      <b className="font-semibold">loves:</b> nature, product design, gym
-    </p>
-    <p className="text-sm mt-3">
-      <b className="font-semibold">born and based in:</b> toronto, CA
-    </p>{" "}
-    <p className="text-sm font-semibold">
-      previously @{" "}
-      <Link
-        href="https://theijf.org"
-        className="font-normal text-sm"
-        target="_blank"
-      >
-        The Investigative Journalism Foundation
-      </Link>
-    </p>
-  </>
-);
+import Link from "next/link";
+import useTypewriter from "@/utils/useTypewriter";
+
+const TRAITS = ["cat mom", "design nerd", "gym rat"];
+
+export const Home = () => {
+  const trait = useTypewriter(TRAITS);
+
+  return (
+    <>
+      {" "}
+      <p className="text-sm uppercas bg-black uppercase w-fit text-off-white px-1 font-semibold mb-2">
+        stephanie li
+      </p>
+      <p className="text-sm">
+        <b className="font-semibold">in summary:</b> a software developer &{" "}
+        {trait}
+        <span className="animate-pulse">|</span>
+      </p>
+      <p className="text-sm">
+        <b className="font-semibold">main proficiencies:</b> typescript, react,
+        python, sql
+      </p>{" "}
+      <p className="text-sm">
+        <b className="font-semibold">loves:</b> nature, product design, gym
+      </p>
+      <p className="text-sm mt-3">
+        <b className="font-semibold">born and based in:</b> toronto, CA
+      </p>{" "}
+      <p className="text-sm font-semibold">
+        previously @{" "}
+        <Link
+          href="https://theijf.org"
+          className="font-normal text-sm"
+          target="_blank"
+        >
+          The Investigative Journalism Foundation
+        </Link>
+      </p>
+    </>
+  );
+};
